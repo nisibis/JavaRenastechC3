86d9633126adf300155d2178fe9a58cb5a9fe805
nisibis <ros47usa@gmail.com>
2021-09-13 19:32:11 -0500

    reverse string

---
+++ src/day12_doWhileLoop/c2_ReverseString.java
@@ -0,0 +1,25 @@
+package day12_doWhileLoop;
+
+public class c2_ReverseString {
+
+    public static void main(String[] args) {
+        //\
+        String actualText = "Java is so fun !!!";
+        System.out.println(actualText);
+        String reverse = "";
+        int lastIndex = actualText.length()-1; //last index of our text which is our first reverse
+
+        do {
+            //our gool is store each letter from last one to first one
+
+            reverse+=actualText.charAt(lastIndex);
+            lastIndex--;
+            //from last index to first index
+        } while (lastIndex>=0); //condition which is your end point which is index 0
+        System.out.println(reverse);
+    }
+
+
+}
+      //
+
